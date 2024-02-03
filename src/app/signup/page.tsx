e9411137a07722298d9eba008325007bfb54037c
davidGBbr <davidgomesbrigido@gmail.com>
2024-02-03 20:30:19 -0300

    Signup page created

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,10 +3,10 @@ import { Input } from "@/components/input";
 import Link from "next/link";
 
 export const metadata = {
-  title: "Mesa Master - Faça seu login",
+  title: "Faça seu cadastro",
 };
 
-export default function Home() {
+export default function SignUp() {
   return (
     <main className="min-h-screen flex justify-center items-center flex-col">
       <h1 className="font-mono font-bold text-white text-7xl tracking-tight">
@@ -14,14 +14,15 @@ export default function Home() {
       </h1>
       <div className="mt-8 w-[90%] sm:w-[600px] flex items-center justify-center flex-col py-8 px-6">
         <form action="" className="w-[90%] flex flex-col">
+          <Input placeholder="Digite seu nome" />
           <Input placeholder="Digite seu email" />
           <Input placeholder="Digite sua senha" />
           <Button loading={false} type="submit">
-            Acessar
+            Cadastrar
           </Button>
         </form>
-        <Link href="/signup" className="mt-4 text-white cursor-pointer">
-          Não possui uma conta? Cadastre-se
+        <Link href="/" className="mt-4 text-white cursor-pointer">
+          Já possui uma conta? Faça login
         </Link>
       </div>
     </main>
