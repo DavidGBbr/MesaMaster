918dd2b85144a016f2bba27923bd3f3d16acbcd5
davidGBbr <davidgomesbrigido@gmail.com>
2024-02-05 17:08:07 -0300

    Logging in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,44 @@
 "use client";
 import { Button } from "@/components/button";
 import { Input } from "@/components/input";
+import { AuthContext } from "@/contexts/AuthContext";
 import Link from "next/link";
+import { FormEvent, useContext, useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const { signIn } = useContext(AuthContext);
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    let data = {
+      email,
+      password,
+    };
+
+    await signIn(data);
+  };
+
   return (
     <main className="min-h-screen flex justify-center items-center flex-col">
       <h1 className="font-mono font-bold text-white text-7xl tracking-tight">
         Mesa<span className="text-cta">Master</span>
       </h1>
       <div className="mt-8 w-[90%] sm:w-[600px] flex items-center justify-center flex-col py-8 px-6">
-        <form className="w-[90%] flex flex-col">
-          <Input placeholder="Digite seu email" />
-          <Input placeholder="Digite sua senha" />
+        <form onSubmit={handleSubmit} className="w-[90%] flex flex-col">
+          <Input
+            placeholder="Digite seu email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="Digite sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
           <Button loading={false} type="submit">
             Acessar
           </Button>
--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,6 @@
 "use client";
+import { api } from "@/services/apiClient";
+import { destroyCookie, setCookie } from "nookies";
 import { PropsWithChildren, createContext, useState } from "react";
 
 interface UserProps {
@@ -16,20 +18,50 @@ interface AuthContextData {
   user: UserProps;
   isAuthenticated: boolean;
   signIn: (credentials: SignInProps) => Promise<void>;
+  signOut: () => void;
 }
 
 export const AuthContext = createContext({} as AuthContextData);
+
+export const signOut = () => {
+  try {
+    destroyCookie(undefined, "@nextauth.token");
+    window.location.href == "/";
+  } catch {
+    console.log("Erro ao deslogar");
+  }
+};
 
 export const AuthProvider = ({ children }: PropsWithChildren) => {
   const [user, setUser] = useState<UserProps>();
   const isAuthenticated = !!user;
 
-  const signIn = async () => {
-    alert("SignIn");
+  const signIn = async ({ email, password }: SignInProps) => {
+    try {
+      const response = await api.post("/session", { email, password });
+      const { id, name, token } = response.data;
+
+      setCookie(undefined, "@nextauth.token", token, {
+        maxAge: 60 * 60 * 24 * 30,
+        path: "/",
+      });
+
+      setUser({
+        id,
+        name,
+        email,
+      });
+
+      api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      window.location.href = "/dashboard";
+    } catch (error) {
+      console.log("Erro ao acessar: " + error);
+    }
   };
 
   return (
-    <AuthContext.Provider value={{ user, isAuthenticated, signIn }}>
+    <AuthContext.Provider value={{ user, isAuthenticated, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
