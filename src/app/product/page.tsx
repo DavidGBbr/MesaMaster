c363a34b273b4f3a83c97a34614a7bc34de660dd
davidGBbr <davidgomesbrigido@gmail.com>
2024-02-14 16:35:27 -0300

    Product photo component created

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,8 +1,29 @@
+"use client";
 import Header from "@/components/header";
 import { Input, TextArea } from "@/components/input";
-import React from "react";
+import React, { ChangeEvent, useState } from "react";
+import { FiUpload } from "react-icons/fi";
 
 const Product = () => {
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const [imageAvatar, setImageAvatar] = useState(null);
+
+  const handleFile = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) {
+      return;
+    }
+
+    const image = e.target.files[0];
+
+    if (!image) {
+      return;
+    }
+
+    if (image.type === "image/jpeg" || image.type === "image/png") {
+      setImageAvatar(image);
+      setAvatarUrl(URL.createObjectURL(e.target.files[0]));
+    }
+  };
   return (
     <>
       <Header />
@@ -10,6 +31,27 @@ const Product = () => {
       <main className="max-w-3xl my-16 mx-auto py-0 px-8 flex justify-between flex-col">
         <h1 className="text-cta text-2xl font-bold">Novo produto</h1>
         <form className="flex flex-col my-4 mx-0">
+          <label className="w-full h-72 bg-slate-950 mb-4 rounded-md flex justify-center items-center cursor-pointer">
+            <span className="z-50 absolute opacity-70 transition-all duration-300 hover:opacity-100 hover:scale-125">
+              <FiUpload size={30} color="#fff" />
+              <input
+                type="file"
+                accept="image/png, image/jpeg"
+                className="hidden"
+                onChange={handleFile}
+              />
+            </span>
+            {avatarUrl && (
+              <img
+                src={avatarUrl}
+                alt="Foto do produto"
+                width={250}
+                height={250}
+                className="w-full h-full object-cover rounded-md"
+              />
+            )}
+          </label>
+
           <select className="mb-4 h-10 rounded-lg bg-slate-950 text-white py-0 px-2 border-[1px] border-gray-600 placeholder:text-slate-300">
             <option>Acompanhamentos</option>
             <option>Porções</option>
